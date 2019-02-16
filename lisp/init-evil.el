9b05781f69936f76ad403a65c60650f310ec7f61
Gabriel Barreto <gabriel.barreto@ethereum.org>
2019-02-16 00:09:24 -0300

    Correction in init-evil.el

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,6 +1,5 @@
-(require 'evil)
-
 (setq evil-want-C-u-scroll 1)
+
 (dolist (mode '(package-menu-mode))
     (add-to-list 'evil-motion-state-modes mode))
 (delete 'package-menu-mode evil-emacs-state-modes)
@@ -13,6 +12,6 @@
   (delete 'term-mode evil-insert-state-modes)
   (delete 'eshell-mode evil-insert-state-modes)
 
+(require 'evil)
 (evil-mode 1)
-
 (provide 'init-evil)
