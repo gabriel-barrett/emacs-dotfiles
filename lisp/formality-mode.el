5796b6e4bbfc1d3fed7fb2b7359fcd17e24b126a
Gabriel Barreto <gabriel.aquino.barreto@gmail.com>
2020-05-28 13:47:14 -0300

    Added comment to syntax table in formality-mode

--- lisp/formality-mode.el
+++ lisp/formality-mode.el
@@ -5,16 +5,24 @@
   "Show the `formality-mode` version in the echo area."
   (interactive "P")
   (let* ((formality-mode-dir (ignore-errors
-                             (file-name-directory (or (locate-library "formality-mode") ""))))
+                               (file-name-directory (or (locate-library "formality-mode") ""))))
          (version (format "formality-mode version %s (%s)"
-                           formality-version
-                           formality-mode-dir)))
+                          formality-version
+                          formality-mode-dir)))
     (if here
         (insert version)
       (message "%s" version))))
 
 (add-to-list 'auto-mode-alist '("\\.fm\\'" . formality-mode))
 (add-to-list 'auto-mode-alist '("\\.fmc\\'" . formality-mode))
+
+(defvar formality-mode-syntax-table nil "Syntax table for `formality-mode'.")
+
+(setq formality-mode-syntax-table
+      (let ( (synTable (make-syntax-table)))
+        (modify-syntax-entry ?/ ". 12b" synTable)
+        (modify-syntax-entry ?\n "> b" synTable)
+        synTable))
 
 (defface formality-number-face
   '((t :foreground "purple"))
@@ -27,17 +35,21 @@
   "Major mode for editing Formality programs."
   :group 'formality
 
+  (set-syntax-table formality-mode-syntax-table)
+
+  ; For comment-dwim
   (setq-local comment-start "// ")
   (setq-local comment-end "")
+
   (font-lock-add-keywords 'nil '(("Nat\\.[0-9]+" . 'formality-number-face)
                                  ("let\\([^=]+?\\)=" . (1 font-lock-function-name-face))
                                  ("def\\([^=]+?\\)=" . (1 font-lock-function-name-face))
                                  ("get\\([^=]+?\\)=" . (1 font-lock-function-name-face))
-                                 (":\\|;\\|=\\|->\\|<\\|>\\|,\\|(\\|)\\||" 0 'font-lock-variable-name-face t)
+                                 (":\\|;\\|=\\|->\\|<\\|>\\|,\\|(\\|)\\||" . 'font-lock-variable-name-face)
                                  ("let" . font-lock-keyword-face)
                                  ("def" . font-lock-keyword-face)
                                  ("get" . font-lock-keyword-face)
-                                 ("//.*" 0 font-lock-comment-face t)))
+                                 ))
   (local-set-key (kbd "C-c C-l")
                  (lambda () (interactive) (async-shell-command "/home/nixos/.npm-global/bin/fm")))
   )
